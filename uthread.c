03835b756bfaaca7b4743f53a311ba804e733f6a
aviad <aviadhadad@gmail.com>
2014-04-28 11:28:49 +0300

    added uthread.c

--- uthread.c
+++ uthread.c
@@ -10,9 +10,112 @@ int
 uthread_init() {
 	t_table = malloc(MAX_THREAD*sizeof(uthread));
 	int i = 0 ;
-	for (i=0;i<MAX_THREAD;i++)
-		t_table[i].state=T_SLEEPING;
+	for (i=0;i<MAX_THREAD;i++) {
+		t_table[i].state=T_FREE;
+		t_table[i].tid=i;
+	}
+	current_thread=0;
+	t_table[0].state=T_RUNNING;
+	t_table[0].stack=0;
 	signal(14,uthread_yield);
-	alarm(5);
+	alarm(THREAD_QUANTA);
 
+}
+
+int
+getNextFreeThread(void) {
+	int i;
+	for (i=0;i<MAX_THREAD;i++) 
+		if (t_table[i].state==T_FREE)
+			return i;
+	return -1;
+}
+
+int
+getNextRunnableThread(void) {
+	int i;
+	for (i=0;i<MAX_THREAD;i++) 
+		if (t_table[(i+1+current_thread)%MAX_THREAD].state==T_RUNNABLE)
+			return (i+1+current_thread)%MAX_THREAD;
+	return -1;
+}
+
+int  
+uthred_self(void) {
+    return current_thread;
+}
+
+
+void 
+uthread_yield(void) {
+	uthread_p this = &t_table[current_thread];
+	this->state=T_RUNNABLE;
+	STORE_ESP(this->esp);
+    STORE_EBP(this->ebp);
+    PUSHAL;
+    int next = getNextRunnableThread();
+    t_table[next].state=T_RUNNING;
+    current_thread=next;
+    LOAD_EBP(t_table[current_thread]->ebp);
+    LOAD_ESP(t_table[current_thread]->esp);
+    POPAL;
+    alarm(THREAD_QUANTA);
+    return ;
+}
+
+int 
+uthread_create(void (*func)(void *), void* arg) {
+	int tid = getNextFreeThread();
+	if (tid==-1) {
+		printf(2,"No more threads can be created\n");
+		exit();
+	}
+	t_table[tid].tid=tid;
+	t_table[tid].state=T_RUNNABLE;
+	t_table[tid].stack=malloc(STACK_SIZE);
+	t_table[tid].stack+STACK_SIZE-4=arg;
+	t_table[tid].stack+STACK_SIZE-8=uthread_exit;
+	t_table[tid].stack+STACK_SIZE-12=0;  //fake old ebp, doesnt matter
+	t_table[tid].stack+STACK_SIZE-16=func;
+	t_table[tid].stack+STACK_SIZE-20=(int)t_table[tid].stack+STACK_SIZE-12     ;//"old ebp" - will go to ebp
+	t_table[tid].stack+STACK_SIZE-24=0; // eax ;
+	t_table[tid].stack+STACK_SIZE-28=0; // ebx
+	t_table[tid].stack+STACK_SIZE-32=0; // ecx
+	t_table[tid].stack+STACK_SIZE-36=0; // edx
+	t_table[tid].stack+STACK_SIZE-40=0; // edi
+	t_table[tid].stack+STACK_SIZE-44=0; // esi
+	t_table[tid].esp=(int)t_table[tid].stack+STACK_SIZE-44;
+	t_table[tid].ebp=(int)t_table[tid].stack+STACK_SIZE-20;  
+	return ;
+}
+
+
+void 
+uthread_exit(void) {
+	free(t_table[current_thread].stack);
+	t_table[current_thread].state=T_FREE;
+
+	int next = getNextRunnableThread();
+	if (next==-1) {
+		printf(2,"last thread finished!\n");
+		exit();
+	}
+    t_table[next].state=T_RUNNING;
+    current_thread=next;
+    LOAD_EBP(t_table[next]->ebp);
+    LOAD_ESP(t_table[next]->esp);
+    POPAL;
+    alarm(THREAD_QUANTA);
+    return ;
+}
+
+int 
+uthred_join(int tid) {
+	if (tid>MAX_THREAD)
+		return 1;
+	while(1) {
+		if (t_table[tid].state==T_FREE)
+			return 0;
+		uthread_yield();
+	}
 }
--- uthread.h
+++ uthread.h
@@ -1,5 +1,29 @@
 #define THREAD_QUANTA 5
 
+
+#define STORE_ESP(var)  asm volatile("movl %%esp, %0;" : "=r" ( var ))
+#define LOAD_ESP(var)   asm volatile("movl %0, %%esp;" : : "r" ( var ))
+#define STORE_EBP(var)  asm volatile("movl %%ebp, %0;" : "=r" ( var ))
+#define LOAD_EBP(var)   asm volatile("movl %0, %%ebp;" : : "r" ( var ))
+#define CALL(addr)              asm volatile("call *%0;" : : "r" ( addr ))
+#define PUSH(var)               asm volatile("movl %0, %%edi; push %%edi;" : : "r" ( var ))
+#define RET                     asm volatile("ret;")
+#define PUSHAL                  asm volatile("push eax;")\
+								asm volatile("push ebx;")\
+								asm volatile("push ecx;")\
+								asm volatile("push edx;")\
+								asm volatile("push edi;")\
+								asm volatile("push esi;")
+
+#define POPAL                   asm volatile("pop esi;")\
+								asm volatile("pop edi;")\
+								asm volatile("pop edx;")\
+								asm volatile("pop ecx;")\
+								asm volatile("pop ebx;")\
+								asm volatile("pop eai;")
+
+
+
 /* Possible states of a thread; */
 typedef enum  {T_FREE, T_RUNNING, T_RUNNABLE, T_SLEEPING} uthread_state;
 
@@ -12,12 +36,6 @@ struct uthread {
 	int				tid;
 	int 	       	esp;        /* current stack pointer */
 	int 	       	ebp;        /* current base pointer */
-	int 			eax;
-	int 			ebx;
-	int 			ecx;
-	int 			edx;
-	int 			esi;
-	int 			edi;
 	char		   *stack;	    /* the thread's stack */
 	uthread_state   state;     	/* running, runnable, sleeping */
 };
